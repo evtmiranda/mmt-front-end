2a14d1e317acf6583adb5059903968db1c0f8946
Everton miranda de oliveira <evt.miranda@outlook.com>
2017-03-25 13:46:13 -0300

    Inclusao da view parcial de edicao de pedidos

---
+++ marmitex/Models/PostAjax.cs
@@ -0,0 +1,9 @@
+﻿namespace marmitex.Models
+{
+    public class PostAjax
+    {
+        public string Recurso { get; set; }
+        public string ViewParcial { get; set; }
+        public string DivASerAtualizada { get; set; }
+    }
+}
