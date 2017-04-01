2ed016b32f0e6d3deaa9f4809a9866dee5adba2d
Everton miranda de oliveira <evt.miranda@outlook.com>
2017-04-01 15:45:06 -0300

    Ajustes de login

--- marmitex/Controllers/LoginController.cs
+++ marmitex/Controllers/LoginController.cs
@@ -24,43 +24,56 @@
             return View();
         }
 
+        /// <summary>
+        /// 1. Verifica se o usuário existe na base
+        /// 2. Busca os dados do usuário e preenche a sessão "usuarioLogado"
+        /// 3. Direciona para a view "Index" do controller "Home"
+        /// </summary>
+        /// <param name="usuario"></param>
+        /// <returns></returns>
         [HttpPost]
         public ActionResult Autenticar(Usuario usuario)
         {
+            //guarda o usuário que está tentando fazer login
+            Session["usuarioLogin"] = usuario;
+
+            //captura a rede de lojas em questão
+            //a rede é utilizada para validar se o usuário existe e se pertence a rede onde está tentando logar
+            Session["dominioRede"] = PreencherSessaoDominioRede();
+
+            //se não conseguir capturar a rede, direciona para a tela de erro
+            if (Session["dominioRede"] == null)
+                return RedirectToAction("Index", "Erro");
+
+            string dominioRede = Session["dominioRede"].ToString();
+
             DadosRequisicaoRest retornoAutenticacao = new DadosRequisicaoRest();
             DadosRequisicaoRest retornoDadosUsuario = new DadosRequisicaoRest();
 
             try
             {
-                retornoAutenticacao = rest.Post("/usuario/autenticar", usuario);
+                string urlPost = string.Format("/usuario/autenticar/{0}/'{1}'", TipoUsuario.Parceiro, dominioRede);
+
+                retornoAutenticacao = rest.Post(urlPost, usuario);
 
                 //se o usuário for autenticado, direciona para a tela home
                 if (retornoAutenticacao.HttpStatusCode == HttpStatusCode.Accepted)
                 {
-                    Usuario usuarioLogado = new Usuario();
+                    UsuarioParceiro usuarioLogado = new UsuarioParceiro();
+
                     try
                     {
                         //busca os dados do usuário
-                        retornoDadosUsuario = rest.PostComRetorno("usuario/buscarPorEmail", usuario);
+                        retornoDadosUsuario = rest.PostComRetorno(string.Format("usuario/buscarPorEmail/{0}", TipoUsuario.Parceiro), usuario);
 
                         //verifica se os dados do usuário foram encontrados
                         if (retornoDadosUsuario.HttpStatusCode != HttpStatusCode.OK)
                             throw new Exception();
 
-                        usuarioLogado = JsonConvert.DeserializeObject<Usuario>(retornoDadosUsuario.objeto.ToString());
+                        usuarioLogado = JsonConvert.DeserializeObject<UsuarioParceiro>(retornoDadosUsuario.objeto.ToString());
 
                         //armazena o usuário na sessão "Usuário"
                         Session["usuarioLogado"] = usuarioLogado;
-
-                        //verifica qual tela chamou o login
-                        if(Session["ControllerDestinoAposLogar"] != null)
-                        {
-                            string controllerDestinho = (string)Session["ControllerDestinoAposLogar"];
-
-                            Session["ControllerDestinoAposLogar"] = null;
-
-                            return RedirectToAction("Index", controllerDestinho);
-                        }
 
                         return RedirectToAction("Index", "Home");
                     }
@@ -68,7 +81,7 @@
                     catch (Exception)
                     {
                         ViewBag.MensagemAutenticacao = "Estamos com dificuldade em buscar dados no servidor. Por favor, tente novamente";
-                        return View();
+                        return View("Index");
                     }
                 }
                 else if (retornoAutenticacao.HttpStatusCode == HttpStatusCode.Unauthorized)
@@ -102,6 +115,16 @@
 
             //Direciona para a tela de login
             return View("Index");
+        }
+
+        /// <summary>
+        /// identifica a rede de lojas pela URL
+        /// </summary>
+        /// <returns></returns>
+        public string PreencherSessaoDominioRede()
+        {
+            //captura o host atual
+            return Request.Url.Host.Replace('"', ' ').Trim();
         }
     }
 }
