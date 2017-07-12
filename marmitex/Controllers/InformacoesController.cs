9518488ef026d758abeb3905ede420b828b938f5
Everton miranda de oliveira <evt.miranda@outlook.com>
2017-07-12 19:33:06 -0300

    inclusao do rodape e do menu informacoes

---
+++ marmitex/Controllers/InformacoesController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace marmitex.Controllers
+{
+    public class InformacoesController : Controller
+    {
+        // GET: Informacoes
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
